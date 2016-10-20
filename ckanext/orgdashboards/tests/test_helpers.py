5e9cd258dd9be67b714b12d7a40c16fd57b6ab8c
Aleksandar Jovanov <aleksandar.jovanov@keitaro.info>
2016-10-20 17:08:27 +0200

    Add more tests for helpers

--- ckanext/orgdashboards/tests/test_helpers.py
+++ ckanext/orgdashboards/tests/test_helpers.py
@@ -6,6 +6,8 @@ from ckan.tests.helpers import reset_db
 from ckan import plugins
 import ckan.lib.search as search
 from ckan.tests import factories
+from ckan.plugins import toolkit
+from pylons import config
 
 from ckanext.orgdashboards.tests.helpers import (id_generator,
                                                  create_mock_data,
@@ -189,3 +191,157 @@ class TestHelpers():
         for i, item in enumerate(mock_map_properties.iteritems()):
             assert map_properties[i]['value'] == item[0]
             assert map_properties[i]['text'] == item[1]
+
+    def test_resource_show_map_properties(self):
+        resource_name = id_generator()
+        resource = upload_json_resource(
+            self.mock_data['dataset_name'],
+            resource_name)
+        map_properties = helpers.orgdashboards_resource_show_map_properties(
+            resource['id'])
+
+        assert len(map_properties) == 4
+
+        for i, item in enumerate(mock_map_properties.iteritems()):
+            assert map_properties[i]['value'] == item[0]
+            assert map_properties[i]['text'] == item[1]
+
+    def test_convert_to_list(self):
+        resource_id = self.mock_data['resource_id']
+        resources = helpers.orgdashboards_convert_to_list(resource_id)
+
+        assert len(resources) == 1
+
+        assert resources[0] == resource_id
+
+        resource_ids = '{' + self.mock_data['resource_id'] + ',' +\
+            self.mock_data['resource_id'] + '}'
+        resources = helpers.orgdashboards_convert_to_list(resource_ids)
+
+        assert len(resources) == 2
+
+        assert resources[0] == resource_id
+        assert resources[1] == resource_id
+
+    def test_get_resource_names_from_ids(self):
+        resource_ids = [self.mock_data['resource_id']]
+        resource_names = helpers.orgdashboards_get_resource_names_from_ids(
+            resource_ids)
+
+        assert len(resource_names) == 1
+
+        assert resource_names[0] == self.mock_data['resource_name']
+
+    def test_smart_truncate(self):
+        text = 'this is some text'
+
+        truncated_text = helpers.orgdashboards_smart_truncate(text)
+
+        assert truncated_text == 'this is some text'
+
+        # Create a 850 characters long string
+        text = 'this is some text' * 50
+
+        truncated_text = helpers.orgdashboards_smart_truncate(text)
+
+        text = text[:794]
+
+        assert truncated_text == text
+
+        text = 'this is some text'
+
+        truncated_text = helpers.orgdashboards_smart_truncate(text, length=12)
+
+        assert truncated_text == 'this is'
+
+    def test_get_secondary_language(self):
+        secondary_language = helpers.orgdashboards_get_secondary_language(
+            self.mock_data['organization_name'])
+
+        assert secondary_language == 'none'
+
+        data_dict = {
+            'id': self.mock_data['organization_id'],
+            'orgdashboards_secondary_language': 'fr'
+        }
+
+        toolkit.get_action('organization_patch')(
+            self.mock_data['context'],
+            data_dict)
+
+        secondary_language = helpers.orgdashboards_get_secondary_language(
+            self.mock_data['organization_name'])
+
+        assert secondary_language == 'fr'
+
+    def test_get_secondary_dashboard(self):
+        secondary_dashboard = helpers.orgdashboards_get_secondary_dashboard(
+            self.mock_data['organization_name'])
+
+        assert secondary_dashboard == 'none'
+
+        data_dict = {
+            'id': self.mock_data['organization_id'],
+            'orgdashboards_secondary_dashboard': 'some_dashboard'
+        }
+
+        toolkit.get_action('organization_patch')(
+            self.mock_data['context'],
+            data_dict)
+
+        secondary_dashboard = helpers.orgdashboards_get_secondary_dashboard(
+            self.mock_data['organization_name'])
+
+        assert secondary_dashboard == 'some_dashboard'
+
+    def test_get_current_url(self):
+        controller =\
+            'ckanext.orgdashboards.controllers.dashboard:DashboardsController'
+        action = 'organization_dashboard'
+        name = self.mock_data['organization_name']
+        page = 5
+
+        current_url = helpers.orgdashboards_get_current_url(
+            page=page,
+            params=[],
+            controller=controller,
+            action=action,
+            name=name,
+            exclude_param='page')
+
+        assert current_url == '/organization/{0}/dashboard?page={1}'.format(
+            self.mock_data['organization_name'],
+            page)
+
+    def test_get_country_short_name(self):
+        country_short_name = helpers.orgdashboards_get_country_short_name('en')
+
+        assert country_short_name == 'Eng'
+
+        country_short_name = helpers.orgdashboards_get_country_short_name('fr')
+
+        assert country_short_name == 'Fre'
+
+    def orgdashboards_get_organization_entity_name(self):
+        entity_name = helpers.orgdashboards_get_organization_entity_name()
+
+        assert entity_name == 'organization'
+
+        config.set('ckanext.orgdashboards.organization_entity_name',
+                   'some_entity')
+
+        entity_name = helpers.orgdashboards_get_organization_entity_name()
+
+        assert entity_name == 'some_entity'
+
+    def orgdashboards_get_group_entity_name(self):
+        entity_name = helpers.orgdashboards_get_group_entity_name()
+
+        assert entity_name == 'group'
+
+        config.set('ckanext.orgdashboards.group_entity_name',
+                   'some_entity')
+
+        entity_name = helpers.orgdashboards_get_group_entity_name()
+
+        assert entity_name == 'some_entity'
