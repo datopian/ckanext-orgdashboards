2f7a5e3228a2daae735465e97bafda940efe7590
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2016-10-27 14:54:01 +0200

    Position map bounds properly

--- ckanext/orgdashboards/fanstatic/map-module.js
+++ ckanext/orgdashboards/fanstatic/map-module.js
@@ -3,9 +3,9 @@ ckan.module('orgdashboards_map', function ($, _) {
 
   return {
     initialize: function () {
-      
+
       ckan.orgdashboards.dashboardmap.init(this.options.id,
-                                      this.options.organizationName,
+                                      this.options.organizationname,
                                       this.options.mapurl,
                                       this.options.color,
                                       this.options.main_property);
